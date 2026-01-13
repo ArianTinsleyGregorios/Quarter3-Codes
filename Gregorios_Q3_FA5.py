bf2d26ee546e717f00438d4cd4df8ef68f20e8db
ArianTinsleyGregorios <ariantinsley.gregorios@smc.pshs.edu.ph>
2026-01-13 21:27:20 +0800

    Challenge task

---
+++ Gregorios_Q3_FA5.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+names = ["Tinsley", "John", "Andrei"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+steps = np.array([
+    [4500, 5200, 4800, 5000, 5300],  
+    [4000, 4100, 3900, 4200, 4600],  
+    [6000, 5800, 5900, 6100, 6200]   
+])
+
+daily_totals = []
+for day_index in range(len(days)):
+    total = 0
+    for person_index in range(len(names)):
+        total += steps[person_index][day_index]
+    daily_totals.append(total)
+print("Total steps per day:")
+for i in range(len(days)):
+    print(days[i], ":", daily_totals[i])
+max_index = daily_totals.index(max(daily_totals))
+print("Most active day overall:")
+print(days[max_index], ": ", daily_totals[max_index], "steps")
